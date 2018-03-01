e668eb0f7bdee8c551a5d034c0586e5925124f27
Alina Leuca <alinaleuca@gmail.com>
2018-03-01 11:29:12 +0100

    add routes for favors

--- app.js
+++ app.js
@@ -10,16 +10,25 @@ const favorRoutes = require('./routes/favors');
 const commentRoutes = require('./routes/comments');
 const authRoutes = require('./routes/auth');
 
+
 app.use(parser.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
 
 
-// ====================
-// PASSPORT CONFIG
 
+
+app.get('/', function(req, res) {
+    res.send('homepage')
+});
+
+app.use('/favors', favorRoutes);
 
 app.listen(process.env.PORT || 5000, function(){
     console.log("server running");
 });
+
+
+// ====================
+// PASSPORT CONFIG
--- routes/favors.js
+++ routes/favors.js
@@ -2,26 +2,65 @@ const express = require('express');
 const app = express();
 let router  = express.Router();
 
-//INDEX - show all favors
+let favors = [
+    {
+        id: 1,
+        description: 'this and this and this and that',
+        user: 1
+    },
 
-router.get('/', function(req, res) {
-    if(err) {
-        console.log(err);
-    } else {
-        res.render('favors/favors');
+    {
+        id: 2,
+        description: 'nightvale',
+        user: 2
+    },
+
+    {
+        id: 3,
+        description: 'wrong description',
+        user: 1
     }
+]
+
+//INDEX - show all favors
+router.get('/', function(req, res) {
+    res.render('favors/favors', { favors });
+});
+
+//CREATE - add new favor to DB
+router.post('/', function(req, res) {
+    res.redirect('/favors');
+      // need to redirect to all favors and display new favor that was just created
+  });
+
+//NEW - show form to create new favor
+router.get('/new', function(req, res) {
+    res.render('favors/new');
+});
+
+// SHOW - shows more info about one favor
+router.get('/:favorId', function(req, res) {
+    res.render('favors/show');
+});
+
+// EDIT FAVOR ROUTE
+router.get('/:favorId/edit', function(req, res) {
+    res.render('favors/edit');
+});
+
+// UPDATE FAVOR ROUTE
+router.put(':/favorId', function (req, res) {
+    res.redirect('favors/' + req.params.id);
+});
+
+// DESTROY FAVOR ROUTE
+router.delete('/:favorId', function(req, res) {
+    res.redirect('/favors');
 });
 
 module.exports = router;
 
-//CREATE - add new favor to DB
 
-//NEW - show form to create new favors
 
-// SHOW - shows more info about one favor
 
-// EDIT FAVOR ROUTE
 
-// UPDATE FAVOR ROUTE
-
-// DESTROY FAVOR ROUTE
