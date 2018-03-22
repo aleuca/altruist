f0c1ce5e44aa61a72691f1b3206bb0f92763aca2
Alina Leuca <alinaleuca@gmail.com>
2018-03-22 14:17:24 +0200

    heroku deploy

--- altruist_database/index.js
+++ altruist_database/index.js
@@ -6,20 +6,27 @@ const path = require('path');
 const pg = require('pg');
 
 
- /**
+/**
  * Modules
  */
 
- const pool = pg.Pool ({
-     user: 'the_altruist',
-     password: 'password',
-     database: 'altruist_database',
-     host: 'altruist_db',
-     port: 5432
- })
+let pool;
+if (process.env.DATABASE_URL) {
+    pool = pg.Pool(process.env.DATABASE_URL);
+} else {
+    // local environment
+    pool = pg.Pool({
+        user: 'the_altruist',
+        password: 'password',
+        database: 'altruist_database',
+        host: 'altruist_db',
+        port: 5432
+    })
+}
 
- /**
+
+/**
  * Module exports
  */
 
- module.exports = pool;
+module.exports = pool;
